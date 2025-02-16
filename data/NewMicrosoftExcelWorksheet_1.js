1e3625437e641bd01d485a6cc00188258f544a37
mhotalebi <157874699+mhotalebi@users.noreply.github.com>
2025-02-16 12:41:43 +0330

    Add files via upload

---
+++ data/NewMicrosoftExcelWorksheet_1.js
@@ -0,0 +1,11 @@
+var json_NewMicrosoftExcelWorksheet_1 = {
+"type": "FeatureCollection",
+"name": "NewMicrosoftExcelWorksheet_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "5-alef", "lat": 29.062817, "long": 52.573678, "grading": 5.0, "marchal": 6.0, "ucs": 2.0, "etc": 8.0 }, "geometry": { "type": "Point", "coordinates": [ 52.573678, 29.062817 ] } },
+{ "type": "Feature", "properties": { "name": "6-alef", "lat": 28.880964, "long": 52.519837, "grading": 7.0, "marchal": 5.0, "ucs": 3.0, "etc": 8.0 }, "geometry": { "type": "Point", "coordinates": [ 52.519837, 28.880964 ] } },
+{ "type": "Feature", "properties": { "name": "8-alef", "lat": 28.770295, "long": 52.038657, "grading": 8.0, "marchal": 4.0, "ucs": 4.0, "etc": 5.0 }, "geometry": { "type": "Point", "coordinates": [ 52.038657, 28.770295 ] } },
+{ "type": "Feature", "properties": { "name": "7-b", "lat": 28.948356, "long": 52.03219, "grading": 10.0, "marchal": 7.0, "ucs": 2.0, "etc": 7.0 }, "geometry": { "type": "Point", "coordinates": [ 52.03219, 28.948356 ] } }
+]
+}
